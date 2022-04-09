46d19e8551449a622a142f0eeaf5603e190c2de8
Bob <bob_w@qq.com>
2022-04-09 14:27:29 +0800

    format

--- src/like_button-repeat.js
+++ src/like_button-repeat.js
@@ -12,7 +12,6 @@ class LikeButton extends React.Component {
     if (this.state.liked) {
       return 'You liked comment number ' + this.props.commentID;
     }
-
     return e(
       'button',
       { onClick: () => this.setState({ liked: true }) },
@@ -21,13 +20,9 @@ class LikeButton extends React.Component {
   }
 }
 
-// Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.like_button_container')
-  .forEach(domContainer => {
-    // Read the comment ID from a data-* attribute.
-    const commentID = parseInt(domContainer.dataset.commentid, 10);
-    ReactDOM.render(
-      e(LikeButton, { commentID: commentID }),
-      domContainer
-    );
-  });
+// Find all DOM containers,and render Like buttons into them.
+document.querySelectorAll('.like_button_container').forEach((domContainer) => {
+  // Read the comment ID from a data-* attribute.
+  const commentID = parseInt(domContainer.dataset.commentid, 10);
+  ReactDOM.render(e(LikeButton, { commentID: commentID }), domContainer);
+});
